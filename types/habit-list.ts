c82fea407995fae38a80a4eca89e64e6de9aef19
Ludmila Lopez <ludmit@outlook.com>
2025-03-29 22:28:37 -0300

    feat: :label: added handleHabit type to habit ListCC

--- types/habit-list.ts
+++ types/habit-list.ts
@@ -3,6 +3,7 @@ import {Habit} from './habit';
 
 export interface HabitListProps {
     habits: Habit[];
+    handleEditHabit:(Habit: Habit) => void;
     toggleComplete: (habitId: number) => void; // Se pasa el ID del hábito
   }
   
