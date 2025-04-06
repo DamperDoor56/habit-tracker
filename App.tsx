305b145a0555b56821938188c42413e5811af217
Ludmila Lopez <ludmit@outlook.com>
2025-04-06 16:21:53 -0300

    feat: :sparkles: add AsyncLocal Storage logic to habits, points goal, animation, etc

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {Habit} from './types/habit';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {GoalCard} from './components/GoalCard';
 import {GoalForm} from './components/GoalForm';
+import {GoalAchievedPopup} from './components/GoalArchived';
 
 export default function Home() {
   const [showAddForm, setShowAddForm] = useState(false);
@@ -16,29 +17,74 @@ export default function Home() {
   const [isEdit, setIsEdit] = useState<boolean>(false);
   const [openGoalForm, setIsOpenGoalForm] = useState<boolean>(false);
   const [goalPoints, setIsGoalPoints] = useState<string>('0');
+  const [hasReachedGoal, setHasReachedGoal] = useState<boolean>(false);
+  const [goalLoaded, setGoalLoaded] = useState<boolean>(false);
 
+  // This useEffect retrieves data from async storage when the app is open
   useEffect(() => {
-    console.log('Loading Habits!');
     const loadHabits = async () => {
       const savedHabits = await AsyncStorage.getItem('habits');
+      const savedDate = await AsyncStorage.getItem('lastResetDate');
+      const savedGoal = await AsyncStorage.getItem('goalPoints');
+      const today = new Date().toDateString();
+
+      if (savedGoal !== null) {
+        // If a goal was saved previously, it sets that value into goalPoints
+        setIsGoalPoints(savedGoal);
+        console.log('🎯 Goal Loaded:', savedGoal);
+      }
+      setGoalLoaded(true);
+
       if (savedHabits) {
-        setHabits(JSON.parse(savedHabits));
+        // If there's saved habits, it retrieves and parse them from JSON so we can use them
+        let parsedHabits = JSON.parse(savedHabits);
+
+        if (savedDate !== today) {
+          // If today’s date is different from when the habit is completed,
+          // it reset all habits (completed: false) .
+          parsedHabits = parsedHabits.map((habit: Habit) => ({
+            ...habit,
+            completed: false,
+          }));
+          await AsyncStorage.setItem('lastResetDate', today);
+          await AsyncStorage.setItem('goalPopupShown', 'false');
+          // Resets the goalPopupShown flag so the popup can show again when the user reaches the goal
+          // next day
+        }
+
+        setHabits(parsedHabits);
+      } else {
+        await AsyncStorage.setItem('lastResetDate', today);
+        await AsyncStorage.setItem('goalPopupShown', 'false');
+        // If there were no saved habits, it reset date and popup flag anyways
       }
     };
 
     loadHabits();
   }, []);
 
+  // Checks and saves habits in Async Storage
   useEffect(() => {
     AsyncStorage.setItem('habits', JSON.stringify(habits));
     console.log('Storing habits!');
   }, [habits]);
 
+  // Checks and saves goals in Async Storage
+  useEffect(() => {
+    if (goalLoaded && goalPoints !== '0') {
+      AsyncStorage.setItem('goalPoints', goalPoints)
+        .then(() => console.log('🎯 goal saved:', goalPoints))
+        .catch(err => console.error('Error al guardar goalPoints:', err));
+    }
+  }, [goalPoints, goalLoaded]);
+
+  // add Habit and closes form
   const addHabit = (habit: Omit<Habit, 'id' | 'completed'>) => {
     setHabits([...habits, {...habit, id: Date.now(), completed: false}]);
     setShowAddForm(false);
   };
 
+  // Complete habit
   const toggleComplete = (id: number) => {
     setHabits(
       habits.map(habit =>
@@ -47,7 +93,7 @@ export default function Home() {
     );
   };
 
-  // Configuración de Tabs
+  // Tabs selection
   const [index, setIndex] = useState(0);
   const [routes] = useState([
     {key: 'all', title: 'Todos'},
@@ -83,6 +129,20 @@ export default function Home() {
     }
     return acc;
   }, 0);
+  useEffect(() => {
+    const checkGoal = async () => {
+      const popupShown = await AsyncStorage.getItem('goalPopupShown');
+      const goal = parseInt(goalPoints, 10);
+      console.log('🎯 Revisando objetivo:', goal, 'Total:', totalPoints);
+
+      if (!isNaN(goal) && totalPoints >= goal && popupShown !== 'true') {
+        setHasReachedGoal(true);
+        await AsyncStorage.setItem('goalPopupShown', 'true');
+      }
+    };
+
+    checkGoal();
+  }, [totalPoints, goalPoints]);
 
   const renderScene = SceneMap({
     all: () => (
@@ -146,6 +206,11 @@ export default function Home() {
           setGoalPoints={setIsGoalPoints}
         />
       )}
+      {/* Success animation*/}
+      <GoalAchievedPopup
+        visible={hasReachedGoal}
+        onClose={() => setHasReachedGoal(false)}
+      />
       {showAddForm ? (
         <AddHabitForm onAdd={addHabit} onCancel={() => setShowAddForm(false)} />
       ) : (
